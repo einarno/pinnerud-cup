6f4721d2d93a8a7dfe8c7ed22289cc0cec61c378
einarno <86827863+einarno@users.noreply.github.com>
2021-08-29 17:47:44 +0200

    Make rating component center

--- web/my-app/src/components/ratingStar.tsx
+++ web/my-app/src/components/ratingStar.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Rating from '@material-ui/lab/Rating';
 import { Box, Grid, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const labels: { [index: string]: string } = {
-    0: 'Forkastelig',
-    0.5: 'Flaut',
-    1: 'Flaut',
-    1.5: 'Flaut+',
-    2: 'Elendig',
-    2.5: 'Elendig+',
+    0: 'Absolutt ubrukelig',
+    0.5: 'Absolutt ubrukelig+',
+    1: 'Forkastelig',
+    1.5: 'Forkastelig+',
+    2: 'Flaut',
+    2.5: 'Flaut+',
     3: 'Dårlig',
     3.5: 'Dårlig+',
     4: 'Begrenset',
@@ -19,15 +18,14 @@ const labels: { [index: string]: string } = {
     5.5: 'OK+',
     6: 'Bra',
     6.5: 'Bra+',
-    7: 'Sterkt',
-    7.5: 'Sterkt',
-    8: 'Imponerende+',
+    7: 'Sterk',
+    7.5: 'Sterk+',
+    8: 'Imponerende',
     8.5: 'Imponerende+',
-    9:  'Overjordisk',
+    9: 'Overjordisk',
     9.5: 'Overjordisk+',
     10: 'Verdensklasse',
-  };
-
+};
 interface raterProps {
     text: string
 }
@@ -35,44 +33,33 @@ export const Rater = (props: raterProps) => {
     const [value, setValue] = React.useState<number | null>(2);
     const [hover, setHover] = React.useState(-1);
     return (
-            <Grid container item justifyContent="center">
-                    <Grid container item justifyContent="center">
-                        <Typography component="legend">{props.text}</Typography>
-                    </Grid>
-                    <Box ml={2}> 
-                        <Rating defaultValue={2} max={10} onChange={(event, newValue) => { setValue(newValue); }}
-                                onChangeActive={(event, newHover) => {setHover(newHover);}}/>
-                    </Box>
-                    {value !== null && <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}
-                
-            </Grid>
+        <Grid container md={4}>
+            <Box width="100%" justifyContent="center">
+                <Typography component="legend">{props.text}</Typography>
+            </Box>
+            <Box pr={3} width="60%" borderColor="transparent" display="flex" flexDirection="row-reverse">
+                <Rating size="small" precision={0.5} defaultValue={2} max={10} onChange={(event, newValue) => { setValue(newValue); }}
+                    onChangeActive={(event, newHover) => { setHover(newHover); }} />
+            </Box >
+            {value !== null && <Box>
+                {labels[hover !== -1 ? hover : value]}
+            </Box>}
+        </Grid >
 
     )
 }
 export const RatingComp = () => {
+    const names: string[] = ["Fart", "Skudd", "Pasninger",
+        "Dribbling", "Forsvar", "Fysikk", "Pinnerud-faktor"]
     return (
-        <Grid container justifyContent="center">
-            <Grid container item> 
-                <Rater text="fart"/>
-            </Grid >
-            <Grid container item> 
-                <Rater text="Skudd"/>
-            </Grid >
-            <Grid container item>  
-                <Rater text="pasninger"/>
-            </Grid>
-            <Grid container item>  
-                <Rater text="dribbling"/>
-            </Grid>
-            <Grid container item>  
-                <Rater text="Forsvar"/>
-            </Grid>
-            <Grid container item>  
-                <Rater text="Fysikk"/>
-            </Grid>
-            <Grid container item>  
-                <Rater text="Pinnerud-faktor"/>
-            </Grid> 
+        <Grid container direction="column">
+            {names.map((value) => {
+                return (
+                    <Grid container direction="row">
+                        <Rater text={value} />
+                    </Grid>)
+            }
+            )}
         </Grid>
     )
 }
